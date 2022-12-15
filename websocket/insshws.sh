9ef735c4f6be9098e9967d2f7f56526b67a4c050
brjalan <74614880+arismaramar@users.noreply.github.com>
2022-12-16 02:42:38 +0700

    Add files via upload

--- websocket/insshws.sh
+++ websocket/insshws.sh
@@ -2,17 +2,17 @@
 
 cd
 
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/DryanZ/multi/aio/websocket/dropbear-ws.py
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/DryanZ/multi/aio/websocket/ws-stunnel
-wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/DryanZ/multi/aio/openvpn/ws-ovpn.py
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/arismaramar/multi/aio/websocket/dropbear-ws.py
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/arismaramar/multi/aio/websocket/ws-stunnel
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/arismaramar/multi/aio/openvpn/ws-ovpn.py
 
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
 chmod +x /usr/local/bin/ws-ovpn
 
-wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/DryanZ/multi/aio/websocket/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
-wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/DryanZ/multi/aio/websocket/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
-wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/DryanZ/multi/aio/openvpn/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/arismaramar/multi/aio/websocket/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/arismaramar/multi/aio/websocket/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/arismaramar/multi/aio/openvpn/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
 
 systemctl daemon-reload
 systemctl enable ws-dropbear.service
